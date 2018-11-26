5a3df1b27e5077cede97b8fde77ac695a04f2316
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-26 16:57:22 +0100

    add klee_make_nondet
    This is the same as klee_make_symbolic, but allows the user to pass
    and identifier other than name that uniquely identifies the call
    to klee_make_nondet.

--- lib/Core/SpecialFunctionHandler.h
+++ lib/Core/SpecialFunctionHandler.h
@@ -120,6 +120,7 @@ namespace klee {
     HANDLER(handleGetValue);
     HANDLER(handleIsSymbolic);
     HANDLER(handleMakeSymbolic);
+    HANDLER(handleMakeNondet);
     HANDLER(handleMalloc);
     HANDLER(handleMemalign);
     HANDLER(handleMarkGlobal);
