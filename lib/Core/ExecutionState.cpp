51b801fccc9eaf80048a776b9dd8f3f9e7fa9b6b
Marek Chalupa <xchalup4@fi.muni.cz>
2018-11-26 16:59:58 +0100

    Remove symbolicNames container
    It is not needed anymore.

--- lib/Core/ExecutionState.cpp
+++ lib/Core/ExecutionState.cpp
@@ -103,7 +103,6 @@ ExecutionState::~ExecutionState() {
 
 ExecutionState::ExecutionState(const ExecutionState& state):
     fnAliases(state.fnAliases),
-    symbolicNames(state.symbolicNames),
     identifiedNondetObjects(state.identifiedNondetObjects),
     pc(state.pc),
     prevPC(state.prevPC),
