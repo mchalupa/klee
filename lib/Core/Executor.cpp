tareq97/JetKlee

Halt the execution on memcleanup error if instructed
We broke it in one of the previous patches.

lib/Core/Executor.cpp
@@ -3521,7 +3521,7 @@ void Executor::terminateStateOnError(ExecutionState &state,
   Instruction * lastInst;
   const InstructionInfo &ii = getLastNonKleeInternalInstruction(state, &lastInst);
 
-  if (CheckMemCleanup) {
+  if (CheckMemCleanup && termReason != Executor::Leak) {
     auto leaks = getMemoryLeaks(state);
     if (!leaks.empty()) {
       std::string info = "";
@@ -3534,7 +3534,8 @@ void Executor::terminateStateOnError(ExecutionState &state,
         klee_message("ERROR: %s:%d: %s", ii.file.c_str(), ii.line, message.c_str());
         reportError(message.c_str(), state, info, suffix, termReason);
       }
-      return terminateState(state);
+      if (shouldExitOn(Executor::Leak))
+        haltExecution = true;
     }
   }
 

mchalupa/klee

lib/Core/Executor.cpp
@@ -4099,7 +4099,7 @@ void Executor::terminateStateOnError(ExecutionState &state,
   Instruction * lastInst;
   const InstructionInfo &ii = getLastNonKleeInternalInstruction(state, &lastInst);
 
-  if (CheckMemCleanup) {
+  if (CheckMemCleanup && terminationType != StateTerminationType::Leak) {
     auto leaks = getMemoryLeaks(state);
     if (!leaks.empty()) {
       std::string info = "";
@@ -4112,7 +4112,8 @@ void Executor::terminateStateOnError(ExecutionState &state,
         klee_message("ERROR: %s:%d: %s", ii.file.c_str(), ii.line, message.c_str());
         reportError(message, state, info, suffix, terminationType);
       }
-      return terminateState(state);
+      if (shouldExitOn(StateTerminationType::Leak))
+        haltExecution = true;
     }
   }
 