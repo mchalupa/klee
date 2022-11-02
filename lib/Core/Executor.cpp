tareq97/JetKlee

Executor: check memcleaup validation only for aborts (and exits)
We checked memcleanup validation upon all errors except Leak.
Change that only to abort() which is what we want, in fact.

Part of a fix for #https://github.com/staticafi/symbiotic/issues/189.

lib/Core/Executor.cpp
@@ -3530,7 +3530,8 @@ void Executor::terminateStateOnError(ExecutionState &state,
   Instruction * lastInst;
   const InstructionInfo &ii = getLastNonKleeInternalInstruction(state, &lastInst);
 
-  if (CheckMemCleanup && termReason != Executor::Leak) {
+  // on abort, we want to report also uncleaned memory
+  if (CheckMemCleanup && termReason == Executor::Abort) {
     auto leaks = getMemoryLeaks(state);
     if (!leaks.empty()) {
       std::string info = "";
@@ -4022,7 +4023,11 @@ Executor::executeAlloc(ExecutionState &state,
       }
     } else {
       ObjectState *os = new ObjectState(*reallocFrom, mo);
-      state.addressSpace.unbindObject(reallocFrom->getObject());
+      auto *oldobj = const_cast<MemoryObject*>(reallocFrom->getObject());
+      state.addressSpace.unbindObject(oldobj);
+      state.addressSpace.removedObjectsMap.insert(
+        std::make_pair(oldobj->segment,
+                       oldobj->getSymbolicAddress(arrayCache)));
       state.addressSpace.bindObject(mo, os);
     }
   }

mchalupa/klee

lib/Core/Executor.cpp
@@ -4102,7 +4102,8 @@ void Executor::terminateStateOnError(ExecutionState &state,
   Instruction * lastInst;
   const InstructionInfo &ii = getLastNonKleeInternalInstruction(state, &lastInst);
 
-  if (CheckMemCleanup && terminationType != StateTerminationType::Leak) {
+  // on abort, we want to report also uncleaned memory
+  if (CheckMemCleanup && terminationType == StateTerminationType::Abort) {
     auto leaks = getMemoryLeaks(state);
     if (!leaks.empty()) {
       std::string info = "";
@@ -4630,7 +4631,10 @@ Executor::executeAlloc(ExecutionState &state,
       }
     } else {
       ObjectState *os = new ObjectState(*reallocFrom, mo);
-      state.addressSpace.unbindObject(reallocFrom->getObject());
+      auto *oldobj = const_cast<MemoryObject*>(reallocFrom->getObject());
+      state.addressSpace.removedObjectsMap.emplace(
+          oldobj->segment, oldobj->getSymbolicAddress(arrayCache));
+      state.addressSpace.unbindObject(oldobj);
       state.addressSpace.bindObject(mo, os);
     }
   }